d4d900c14207674d4a493175e1b9eb941089ba7b
tronin1000 <tigoventuratec@gmail.com>
2024-11-07 22:04:18 -0300

    atualizacao

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.texto}> Ah ce reparou</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f9f',
     alignItems: 'center',
     justifyContent: 'center',
+   
   },
+
+  texto: {
+    color: 'green',
+    fontSize: 50,
+  }
 });
